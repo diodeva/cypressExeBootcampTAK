9b229f5df69f874f09dd1e80d8d613fc42dfb47e
diodev <devlinedio@gmail.com>
2023-04-08 11:35:54 +0700

    update dashboard

--- cypress/e2e/3-exercise/login.cy.js
+++ cypress/e2e/3-exercise/login.cy.js
@@ -5,6 +5,18 @@ function generateRandomUsername() {
   return username;
 }
 
+function generatePhoneNumber() {
+  const randomDigits = Math.floor(Math.random() * 1000000000); // Menghasilkan angka acak dengan 9 digit
+  const phoneNumber = `62${randomDigits.toString().padStart(9, '0')}`; // Memformat nomor telepon
+  return phoneNumber;
+}
+
+function generateRandomEmail() {
+  const randomEmail = Math.random().toString(36).substring(2, 15) + '@example.com' // Generates a random email
+  return randomEmail;
+}
+
+
 
 // describe('Sign up Itera', () => {
 
@@ -195,8 +207,17 @@ describe('Dashboard Itera', () => {
     cy.get('#Username').type('firstQA')
     cy.get('#Password').type('qwerty123')
     cy.get('.btn-primary').click() //button login
+    cy.get('a[href*="/Customer/Create"]').click()
     cy.get('#Name').type('QA ' + username)
-    cy.get('')
+    cy.get('#Company').type('Kemenkominfo Indonesia')
+    cy.get('#Address').type('Indonesia')
+    cy.get('#City').type('Jakarta')
+    cy.get('#Phone').type('0811111111')
+    cy.get('#Email').type(username + '@mail.co.id')
+    cy.get('.btn.btn-primary').click()
+    cy.get('#searching').type('QA ' + username)
+    cy.get('input[value="Search"]').filter('[type="submit"]').click();
+    cy.get('.table.table-hover').should('contain.text', username)
     cy.get('a[href*="/Login/LogOut"]').click()
     
 
@@ -204,31 +225,99 @@ describe('Dashboard Itera', () => {
   })
 
   it('positive - search customer details by name', () => {
+    const username = generateRandomUsername();
     cy.visit('https://itera-qa.azurewebsites.net/')
     cy.get('a[href*="/Login"]').click()
     cy.get('#Username').type('firstQA')
     cy.get('#Password').type('qwerty123')
-    cy.get('.btn-primary').click()
+    cy.get('.btn-primary').click() //button login
+    cy.get('a[href*="/Customer/Create"]').click()
+    cy.get('#Name').type('QA ' + username)
+    cy.get('#Company').type('Kemenkominfo Indonesia')
+    cy.get('#Address').type('Indonesia')
+    cy.get('#City').type('Jakarta')
+    cy.get('#Phone').type('0811111111')
+    cy.get('#Email').type(username + '@mail.co.id')
+    cy.get('.btn.btn-primary').click()
+    cy.get('#searching').type('QA ' + username)
+    cy.get('input[value="Search"]').filter('[type="submit"]').click();
+    cy.get('.table.table-hover').should('contain.text', username)
+    cy.get('a[href*="/Login/LogOut"]').click()
 
+
+  })
+
+  it('positive - search customer details by phone number', () => {
+    const username = generateRandomUsername()
+    const phoneNumber = generatePhoneNumber()
+    cy.visit('https://itera-qa.azurewebsites.net/')
+    cy.get('a[href*="/Login"]').click()
+    cy.get('#Username').type('firstQA')
+    cy.get('#Password').type('qwerty123')
+    cy.get('.btn-primary').click() //button login
+    cy.get('a[href*="/Customer/Create"]').click()
+    cy.get('#Name').type('QA ' + username)
+    cy.get('#Company').type('Kemenkominfo Indonesia')
+    cy.get('#Address').type('Indonesia')
+    cy.get('#City').type('Jakarta')
+    cy.get('#Phone').type(phoneNumber)
+    cy.get('#Email').type(username + '@mail.co.id')
+    cy.get('.btn.btn-primary').click() // button create
+    cy.get('#searching').type(phoneNumber)
+    cy.get('input[value="Search"]').filter('[type="submit"]').click() //button search
+    cy.get('.table.table-hover').should('contain.text', phoneNumber)
+    cy.get('a[href*="/Login/LogOut"]').click()
 
   })
 
   it('positive - search customer details by email', () => {
+    const username = generateRandomUsername()
+    const phoneNumber = generatePhoneNumber()
+    const emails = generateRandomEmail()
     cy.visit('https://itera-qa.azurewebsites.net/')
     cy.get('a[href*="/Login"]').click()
     cy.get('#Username').type('firstQA')
     cy.get('#Password').type('qwerty123')
-    cy.get('.btn-primary').click()
-
+    cy.get('.btn-primary').click() //button login
+    cy.get('a[href*="/Customer/Create"]').click()
+    cy.get('#Name').type('QA ' + username)
+    cy.get('#Company').type('Kemenkominfo Indonesia')
+    cy.get('#Address').type('Indonesia')
+    cy.get('#City').type('Jakarta')
+    cy.get('#Phone').type(phoneNumber)
+    cy.get('#Email').type(emails)
+    cy.get('.btn.btn-primary').click() // button create
+    cy.get('#searching').type(emails)
+    cy.get('input[value="Search"]').filter('[type="submit"]').click() //button search
+    cy.get('.table.table-hover').should('contain.text', emails)
+    cy.get('a[href*="/Login/LogOut"]').click()
 
   })
 
-  it('positive - delete customer details', () => {
+  it.only('positive - delete customer details', () => {
+    const username = generateRandomUsername()
+    const phoneNumber = generatePhoneNumber()
+    const emails = generateRandomEmail()
     cy.visit('https://itera-qa.azurewebsites.net/')
     cy.get('a[href*="/Login"]').click()
     cy.get('#Username').type('firstQA')
     cy.get('#Password').type('qwerty123')
-    cy.get('.btn-primary').click()
+    cy.get('.btn-primary').click() //button login
+    cy.get('a[href*="/Customer/Create"]').click()
+    cy.get('#Name').type('QA ' + username)
+    cy.get('#Company').type('Kemenkominfo Indonesia')
+    cy.get('#Address').type('Indonesia')
+    cy.get('#City').type('Jakarta')
+    cy.get('#Phone').type(phoneNumber)
+    cy.get('#Email').type(emails)
+    cy.get('.btn.btn-primary').click() // button create
+    cy.get('#searching').type(emails)
+    cy.get('input[value="Search"]').filter('[type="submit"]').click() //button search
+    cy.get('.table.table-hover').should('contain.text', emails)
+    cy.get('a[href*="/Customer/Edit"]').click()
+    cy.get('').type('')
+    cy.get('').type('')
+    cy.get('a[href*="/Login/LogOut"]').click()
 
 
   })
@@ -236,22 +325,14 @@ describe('Dashboard Itera', () => {
   it('positive - edit customer details', () => {
     cy.visit('https://itera-qa.azurewebsites.net/')
     cy.get('a[href*="/Login"]').click()
-    cy.get('#Username').type('firstQA')
-    cy.get('#Password').type('qwerty123')
+    cy.get('#Username').clear().type('firstQA')
+    cy.get('#Password').clear().type('qwerty123')
     cy.get('.btn-primary').click()
 
 
   })
 
-  it('negative - search customer details by phone', () => {
-    cy.visit('https://itera-qa.azurewebsites.net/')
-    cy.get('a[href*="/Login"]').click()
-    cy.get('#Username').type('firstQA')
-    cy.get('#Password').type('qwerty123')
-    cy.get('.btn-primary').click()
 
-
-  })
 })
 
 
